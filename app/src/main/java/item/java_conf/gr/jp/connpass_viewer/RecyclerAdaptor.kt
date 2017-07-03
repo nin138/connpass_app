e8151728421fe590bd982903f4ad25e62fef57d3
nin <ninian138@gmail.com>
2017-07-03 11:58:03 +0900

    fc

---
+++ app/src/main/java/item/java_conf/gr/jp/connpass_viewer/RecyclerAdaptor.kt
@@ -0,0 +1,94 @@
+package item.java_conf.gr.jp.connpass_viewer
+
+import android.content.Context
+import android.graphics.Color
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import item.java_conf.gr.jp.connpass_viewer.entity.Event
+
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+class RecyclerAdapter(private val context: Context, private var list: Array<Event>) : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>(), View.OnClickListener {
+  interface OnItemClickListener {
+    fun onItemClick(adapter: RecyclerAdapter, position: Int, event: Event)
+  }
+
+  private var recycler: RecyclerView? = null
+  private val inflater: LayoutInflater = LayoutInflater.from(context)
+  private var clickListener: OnItemClickListener? = null
+  private val dateFmt = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX", Locale.US)
+  private val calender = Calendar.getInstance()
+  private val days = arrayOf("日", "月", "火", "水", "木", "金", "土")
+
+
+  fun setList(list: Array<Event>) {
+    this.list = list
+    recycler?.adapter = this
+  }
+
+  fun setOnItemClickListener(listener: OnItemClickListener) {
+    clickListener = listener
+  }
+
+  override fun onClick(view: View) {
+    if (recycler != null && clickListener != null) {
+      val position = recycler!!.getChildAdapterPosition(view)
+      clickListener?.onItemClick(this, position, list[position])
+    }
+  }
+
+  override fun onAttachedToRecyclerView(recyclerView: RecyclerView?) {
+    super.onAttachedToRecyclerView(recyclerView)
+    recycler = recyclerView
+  }
+
+  override fun onDetachedFromRecyclerView(recyclerView: RecyclerView?) {
+    super.onDetachedFromRecyclerView(recyclerView)
+    recycler = null
+  }
+
+
+  override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ViewHolder {
+    val view = inflater.inflate(R.layout.list_item, viewGroup, false)
+    view.setOnClickListener(this)
+    return ViewHolder(view)
+  }
+
+
+  override fun onBindViewHolder(viewHolder: ViewHolder, i: Int) {
+    if (list.size > i) {
+      viewHolder.series.text = list[i].series.title
+      viewHolder.event.text = list[i].title
+      val num = list[i].accepted + list[i].waiting
+      if(num >= list[i].limit) viewHolder.capacity.setTextColor(Color.RED)
+      else viewHolder.capacity.setTextColor(Color.BLACK)
+      viewHolder.capacity.text = context.getString(R.string.capacity, num ,list[i].limit)
+      val st = list[i].started_at
+      calender.time = dateFmt.parse(st)
+      viewHolder.year.text = st.substring(0, 4)
+      viewHolder.date.text = context.getString(R.string.date, st.substring(5, 7), st.substring(8, 10))
+      viewHolder.day.text = context.getString(R.string.day, days[calender.get(Calendar.DAY_OF_WEEK)-1])
+      viewHolder.time.text = context.getString(R.string.time, st.substring(11, 16))
+    }
+  }
+
+  override fun getItemCount(): Int {
+    return list.size
+  }
+
+  inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val series: TextView = itemView.findViewById(R.id.series_name)
+    val event: TextView = itemView.findViewById(R.id.event_name)
+    val capacity: TextView = itemView.findViewById(R.id.capacity)
+    val year: TextView = itemView.findViewById(R.id.year)
+    val date: TextView = itemView.findViewById(R.id.date)
+    val day: TextView= itemView.findViewById(R.id.day)
+    val time: TextView = itemView.findViewById(R.id.time)
+  }
+
+}
---
+++ app/src/main/java/item/java_conf/gr/jp/connpass_viewer/entity/ConnpassResponse.kt
@@ -0,0 +1,50 @@
+package item.java_conf.gr.jp.connpass_viewer.entity
+
+data class ConnpassQuery(
+        var event_id: Int,
+        var keyword: String,
+        var keyword_or: String,
+        var ym: Int,
+        var ymd: Int,
+        var nickname: String,
+        var owner_nickname: String,
+        var series_id: Int,
+        var start: Int,
+        var order: Int,
+        var count: Int,
+        var format: String
+)
+data class ConnpassResponse(
+        var results_returned: Int,//	整数	含まれる検索結果の件数	1
+        var results_available: Int, //	整数	検索結果の総件数	191
+        var results_start: Int, //	整数	検索の開始位置	1
+        var events: Array<Event>//	配列(複数要素)	検索結果のイベントリスト
+)
+data class Event(
+        var event_id: Int, //	整数	イベントID	364
+        var title: String, //	文字列(UTF-8)	タイトル	BPStudy#56
+        var catch: String, //	文字列(UTF-8)	キャッチ	株式会社ビープラウドが主催するWeb系技術討論の会
+        var description: String, //	文字列(UTF-8)	概要(HTML形式)	今回は「Python プロフェッショナル　プログラミング」執筆プロジェクトの継続的ビルドについて、お話しして頂きます。
+        var event_url: String, //	文字列(UTF-8)	connpass.com 上のURL	https://connpass.com/event/364/
+        var hash_tag: String, //	文字列(UTF-8)	Twitterのハッシュタグ	bpstudy
+        var started_at: String, //	文字列(UTF-8)	イベント開催日時 (ISO-8601形式)	2012-04-17T18:30:00+09:00
+        var ended_at: String, //	文字列(UTF-8)	イベント終了日時 (ISO-8601形式)	2012-04-17T20:30:00+09:00
+        var limit: Int, //	整数	定員	80
+        var event_type: String, //	文字列(UTF-8)	イベント参加タイプ	participation: connpassで参加受付あり,advertisement: 告知のみ
+        var series: Series, //	オブジェクト	グループ
+        var address: String, //	文字列(UTF-8)	開催場所	東京都渋谷区千駄ヶ谷5-32-7
+        var place: String, //	文字列(UTF-8)	開催会場	BPオフィス (NOF南新宿ビル4F)
+        var lat: Double, //	浮動小数点数	開催会場の緯度	35.680236100000
+        var lon: Double, //	浮動小数点数	開催会場の経度	139.701308500000
+        var owner_id: Int, //	整数	管理者のID	8
+        var owner_nickname: String, //	文字列(UTF-8)	管理者のニックネーム	haru860
+        var owner_display_name: String, //	文字列(UTF-8)	管理者の表示名	佐藤 治夫
+        var accepted: Int, //	整数	参加者数	80
+        var waiting: Int, //	整数	補欠者数	15
+        var updated_at: String
+)
+data class Series(
+        var id: Int, //	整数	グループID	1
+        var title: String, //	文字列(UTF-8)	グループタイトル	BPStudy
+        var url: String //文字列(UTF-8)	グループのconnpass.com 上のURL	https://connpass.com/series/1/
+)
